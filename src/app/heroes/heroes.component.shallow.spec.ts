c201f517557c9d90ac6ec1572f4191add60ca589
vincentLirette <vi.lirette@gmail.com>
2019-01-21 17:05:55 -0500

    Mocking a Child Component

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "../hero.service";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, Input, Component } from "@angular/core";
 import { of } from "rxjs";
 
 describe('HeroesComponent (shallow tests)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES;
+
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+      })
+      class FakeHeroComponent {
+        @Input() hero;
+        //@Output() delete = new EventEmitter();
+      
+      }
 
     beforeEach(() =>{
         HEROES = [
@@ -18,11 +28,14 @@ describe('HeroesComponent (shallow tests)', () => {
         mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [
+                HeroesComponent,
+                FakeHeroComponent
+            ],
             providers: [
                 { provide: HeroService, useValue: mockHeroService }
             ],
-            schemas: [NO_ERRORS_SCHEMA]
+            //schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
     });
