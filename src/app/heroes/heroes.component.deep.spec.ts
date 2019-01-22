4062fb0020ae5dc82b22a0f2cf03db5033e15077
vllirette <vi.lirette@gmail.com>
2019-01-22 17:05:27 -0500

    Mocking the RouterLink

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -2,9 +2,22 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "../hero.service";
 import { HeroComponent } from "../hero/hero.component";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, Directive, Input } from "@angular/core";
 import { of } from "rxjs";
 import { By } from "@angular/platform-browser";
+
+@Directive({
+    selector: '[routerLink]',
+    host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub{
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+    
+    onClick(){
+        this.navigatedTo = this.linkParams;
+    }
+}
 
 describe('HeroesComponent (deep tests)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
@@ -23,12 +36,13 @@ describe('HeroesComponent (deep tests)', () => {
         TestBed.configureTestingModule({
             declarations: [
                 HeroesComponent,
-                HeroComponent
+                HeroComponent,
+                RouterLinkDirectiveStub
             ],
             providers: [
                 { provide: HeroService, useValue: mockHeroService }
             ],
-            schemas: [NO_ERRORS_SCHEMA]
+            //schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
         mockHeroService.getHeroes.and.returnValue(of(HEROES));
