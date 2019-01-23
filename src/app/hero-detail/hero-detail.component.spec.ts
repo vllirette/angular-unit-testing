27c8da427096d8bb4dc8641d5a41214e41082037
vllirette <vi.lirette@gmail.com>
2019-01-23 10:29:48 -0500

    Using fakeAsync Helper function

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestBed, ComponentFixture, fakeAsync, tick, flush } from "@angular/core/testing";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
@@ -36,17 +36,16 @@ describe('HeroDetailComponent', () => {
         expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
     });
 
-    it('should call updateHero when save() is called', (done) => {
+    it('should call updateHero when save() is called', fakeAsync(() => {
         mockHeroService.updateHero.and.returnValue(of({}));
         fixture.detectChanges();
 
         fixture.componentInstance.save();
+        //tick(250);
+        flush();
 
-        setTimeout(() =>{
-            expect(mockHeroService.updateHero).toHaveBeenCalled();
-            done();
-        }, 300);
-    });
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+    }));
     
     
 });
